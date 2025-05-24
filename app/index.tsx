ca50480b7f8f1e227dec90c4195b541ab8d906da
Anjany Pandey <anjany.pandey06@gmail.com>
2025-05-24 17:03:35 +0530

    remove unused components and files; simplify layout structure

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "red",
+        flex: 1, // Ensures the view takes up the full screen
+        justifyContent: "center", // Centers content vertically ( y-axis)
+        alignItems: "center", // Centers content horizontally ( x-axis)
+      }}
+    >
+      <Text
+        style={{
+          color: "white",
+          fontSize: 20,
+          fontWeight: "bold",
+          textAlign: "center",
+        }}
+      >
+        hello world
+      </Text>
+    </View>
+  );
+};
+export default Index;
